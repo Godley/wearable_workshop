df6461b7295fc2e82d5a6ca6838452e3976fbd42
Charlotte <charlotte.godley@hotmail.co.uk>
2014-09-14 02:02:24 +0100

    Added a new library, which wraps around the adafruit librar

---
+++ lib/PixStrip.cpp
@@ -0,0 +1,39 @@
+
+#include <Adafruit_NeoPixel.h>
+
+#include "PixStrip.h"
+
+
+Pixels::Pixels(uint16_t n, uint8_t p, uint8_t t,uint32_t col) : Adafruit_NeoPixel(n,p,t)
+{
+  int *_lights=new int[n];
+  for(int i=0;i<n;i++)
+  {
+    _lights[i]=col;
+  }
+  begin();
+  setBrightness(40); // 1/3 brightness
+  uint8_t i=0;
+  for(i=0;i<n;i++)
+    {
+    setPixelColor(i,_lights[i]);
+    }
+  show();
+    
+};
+void Pixels::SetCol(int id, int col)
+{
+  _lights[id]=col;
+};
+
+void Pixels::UpdateLight(int id)
+{
+  setPixelColor(id,_lights[id]);
+  show();
+};
+
+void Pixels::SetBright(int bright)
+{
+  setBrightness(bright);
+  show();
+}
---
+++ lib/PixStrip.h
@@ -0,0 +1,21 @@
+#include "Adafruit_NeoPixel.h"
+#include "Arduino.h"
+#include <unwind-cxx.h>
+#include <StandardCplusplus.h>
+#include <system_configuration.h>
+#include <utility.h>
+#include <vector>
+
+using namespace std;
+
+class Pixels : Adafruit_NeoPixel
+{
+  public:
+    Pixels(uint16_t n, uint8_t p, uint8_t t, uint32_t col);
+    void SetCol(int id, int col);
+    void UpdateLight(int id);
+    void SetBright(int bright);
+  private:
+    int *_lights;
+    int _pin; 
+};
